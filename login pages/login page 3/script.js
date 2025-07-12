91ae8af92e624079c9c3f2c4d70878ac52c06884
Khushi-singhal8 <khushisinghal081@gmail.com>
2025-07-12 23:45:42 +0530

    Delete login pages directory

--- login pages/login page 3/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loginBox = document.getElementById("loginBox");
-  const loginForm = document.getElementById("loginForm");
-
-  loginBox.addEventListener("click", () => {
-    loginForm.classList.add("active");
-  });
-});
